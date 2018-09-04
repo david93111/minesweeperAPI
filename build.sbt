c31a3338e96b82b4fde969132ccf3d3bc114d8c9
DavidVasquez <david93111@gmail.com>
2018-09-04 20:11:02 -0300

    advance on akka persistence for game persistence and game movement history as event-sourcing

--- build.sbt
+++ build.sbt
@@ -16,7 +16,8 @@ val akkaHttpVersion = "10.1.3"
 val akkaVersion = "2.5.14"
 val akkaDependencies = Seq(
   "com.typesafe.akka" %% "akka-stream",
-  "com.typesafe.akka" %% "akka-actor"
+  "com.typesafe.akka" %% "akka-actor",
+  "com.typesafe.akka" %% "akka-persistence"
 ).map(_ % akkaVersion)
 
 // CIRCE
@@ -27,15 +28,22 @@ val circeDependencies = Seq(
   "io.circe" %% "circe-parser"
 ).map(_ % circeVersion)
 
+// Reactive Mongo version
+val rxMongoVersion="0.16.0"
+
 val generalDependencies = Seq(
-  "org.scalatest"       %%  "scalatest"           % "3.0.5"          % Test,
-  "com.typesafe.akka"   %%  "akka-testkit"        % "2.5.14"         % Test,
-  "com.typesafe.akka"   %%  "akka-http-testkit"   % akkaHttpVersion  % Test,
-  "com.typesafe.akka"   %%  "akka-http"           % akkaHttpVersion,
-  "de.heikoseeberger"   %%  "akka-http-circe"     % "1.21.0",
-  "ch.qos.logback"      %   "logback-classic"     % "1.2.3",
-  "com.typesafe"        %   "config"              % "1.3.2",
-  "com.beachape"        %%  "enumeratum-circe"    % "1.5.13"
+  "org.scalatest"          %%   "scalatest"                      % "3.0.5"          % Test,
+  "com.typesafe.akka"      %%   "akka-testkit"                   % "2.5.14"         % Test,
+  "com.typesafe.akka"      %%   "akka-http-testkit"              % akkaHttpVersion  % Test,
+  "com.github.dnvriend"    %%   "akka-persistence-inmemory"      % "2.5.1.1"        % Test,
+  "com.typesafe.akka"      %%   "akka-http"                      % akkaHttpVersion,
+  "de.heikoseeberger"      %%   "akka-http-circe"                % "1.21.0",
+  "ch.qos.logback"         %    "logback-classic"                % "1.2.3",
+  "com.typesafe"           %    "config"                         % "1.3.2",
+  "com.beachape"           %%   "enumeratum-circe"               % "1.5.13",
+  "com.github.scullxbones" %%   "akka-persistence-mongo-rxmongo" % "2.0.11",
+  "org.reactivemongo"      %%   "reactivemongo"                  % rxMongoVersion,
+  "org.reactivemongo"      %%   "reactivemongo-akkastream"       % rxMongoVersion
 )
 
 parallelExecution in Test:=false
--- src/main/scala/co/com/minesweeper/actor/GameActor.scala
+++ src/main/scala/co/com/minesweeper/actor/GameActor.scala
@@ -1,76 +1,107 @@
 package co.com.minesweeper.actor
 
 import akka.actor.Props
+import akka.persistence.{PersistentActor, SnapshotOffer}
 import co.com.minesweeper.actor.GameActor.{GetMinefield, MarkSpot, MinefieldOperation, RevealSpot}
 import co.com.minesweeper.api.services.MinefieldService
 import co.com.minesweeper.model._
 import co.com.minesweeper.model.error.GameOperationFailed
 
-class GameActor(val id: String, minefieldConf: MinefieldConfig, val user: String) extends BaseActor {
+class GameActor(val id: String, minefieldConf: MinefieldConfig, val user: String) extends PersistentActor with BaseActor {
 
-  val minefield: Minefield = MinefieldService.createMinefield(minefieldConf.columns, minefieldConf.rows, minefieldConf.mines)
+  override def persistenceId: String = id
 
-  var status: GameStatus = GameStatus.Active
+  var state = Game(id, GameStatus.Active, user, MinefieldService.createMinefield(minefieldConf.columns, minefieldConf.rows, minefieldConf.mines))
+
+  persistAsync(state)
+  saveSnapshot(state)
+  if (lastSequenceNr % GameActor.snapShotInterval == 0 && lastSequenceNr != 0)
+    saveSnapshot(state)
+
+  var movementHistory: List[Game] = List.empty[Game]
 
   private val cellsToReveal: Int = (minefieldConf.rows * minefieldConf.columns) - minefieldConf.mines
 
   private var revealedCells: Int = 0
 
-  val revealFunction: (Int, Int, Field) => Unit = MinefieldService.revealSpot(minefield.board)
+  val revealFunction: (Int, Int, Field) => Unit = MinefieldService.revealSpot(state.minefield.board)
 
   private val validateCellOperation: (Int, Int) => Boolean = MinefieldService.cellInValidRange(minefieldConf.columns, minefieldConf.rows)
 
   def minefieldOperationsReceive: Receive = {
     case RevealSpot(row, col) =>
-      val field: Field = minefield.board(row)(col)
+      val field: Field = state.minefield.board(row)(col)
       revealSpot(row, col, field)
       field.fieldType match {
         case FieldType.Mine =>
-          status = GameStatus.Lose
+          updateState(GameStatus.Lose)
         case FieldType.Hint =>
           revealedCells = revealedCells + 1
           validateIfWon()
         case FieldType.Empty =>
-          val quantityRevealed =  MinefieldService.revealSpotsUntilHintOrMine(minefield.board, MinefieldService.nearSpots(row, col),validateCellOperation)
+          val quantityRevealed = MinefieldService.revealSpotsUntilHintOrMine(state.minefield.board, MinefieldService.nearSpots(row, col),validateCellOperation)
           revealedCells = revealedCells + 1 + quantityRevealed
           validateIfWon()
       }
-      sender() ! Game(id, status, user, minefield)
+      sender() ! state
     case MarkSpot(row, col, mark) =>
-      val field: Field = minefield.board(row)(col)
+      val field: Field = state.minefield.board(row)(col)
       if (!field.revealed || mark == MarkType.None){
-        minefield.board(row)(col) = field.copy(mark = mark)
-        sender() ! Game(id, status, user, minefield)
+        state.minefield.board(row)(col) = field.copy(mark = mark)
+        sender() ! state
       } else sender() ! GameOperationFailed.cellAlreadyRevealed(id)
   }
 
-  override def receive: Receive = {
+  override def receiveRecover: Receive = {
+    case game: Game                                 ⇒ updateState(game)
+    case SnapshotOffer(_, snapshot: ) ⇒ state = snapshot
+  }
+
+  override def receiveCommand: Receive = {
     case GetMinefield =>
-      sender() ! Game(id, status, user, minefield)
+      sender() ! state
     case e: MinefieldOperation =>
       if(!validateCellOperation(e.row, e.column))
         sender() ! GameOperationFailed.invalidCell(id)
-      else if(status != GameStatus.Active)
+      else if(state.gameStatus != GameStatus.Active)
         sender() ! GameOperationFailed.GameFinished(id)
       else
         minefieldOperationsReceive(e)
   }
 
+  private def updateState(gameStatus: GameStatus): Unit ={
+      movementHistory = state :: movementHistory
+      state = state.copy(gameStatus = gameStatus)
+  }
+
+  private def uptadeState(newState: Game): Unit = {
+      movementHistory = state :: movementHistory
+      state = newState
+  }
+
   private def validateIfWon(): Unit = {
-    if(revealedCells == cellsToReveal) status = GameStatus.Won
+    if(revealedCells == cellsToReveal) updateState(GameStatus.Won)
   }
 
   private def revealSpot(row: Int, col: Int, field: Field): Unit ={
     revealFunction(row, col, field)
   }
+
+
+
 }
 
 object GameActor{
 
+  val snapShotInterval = 1000
   case object GetMinefield
   abstract class MinefieldOperation(val row: Int, val column: Int)
   case class RevealSpot(override val row: Int, override val column: Int) extends MinefieldOperation(row, column)
   case class MarkSpot(override val row: Int, override val column: Int, mark: MarkType) extends MinefieldOperation(row, column)
+
+  case class GameState(movementHistory: List[Game]){
+
+  }
 
   def props( id: String, minefieldConf: MinefieldConfig, user: String): Props = {
     Props(new GameActor(id, minefieldConf, user))
